f86e873921e53afa48aa1278610073f7151c573e
Richterite <fikrifauzy156@gmail.com>
2026-08-05 17:06:17 +0700

    ✨feat(theme): Add Color.kt for donut char color theme

---
+++ app/src/main/java/com/example/ulyssespact/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.ulyssespact.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object  ChartPalettes {
+    val Default = listOf<Color>(
+        Color(0xFF6200EA), // Purple
+        Color(0xFF03DAC5), // Teal
+        Color(0xFFFF0266), // Pink
+        Color(0xFFFFC107), // Amber
+        Color(0xFF2196F3), // Blue
+        Color(0xFF4CAF50)  // Green
+    )
+}
